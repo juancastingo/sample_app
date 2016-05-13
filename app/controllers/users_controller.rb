25c2c3199e0fe53ed53318ec8ee74050b643b22d
Juan Cruz Stingo <stingo@JuancapcT430.argentina.ibm.com>
2016-05-13 00:07:20 -0300

    Use SSL and the Puma webserver in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  def show
+    @user = User.find(params[:id])
+    #debugger
+    @parametros = params
+  end
+
+ 
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    Rails.logger.debug "\033[1;34;40m[DEBUG] Juanca-DEBUG: \033[0m " + @user.to_s
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # igual a redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  resources :users
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
